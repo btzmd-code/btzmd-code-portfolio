6934ad1d9e395abb3bc6a59c92ccecbe8e6f6ead
btzmd <163552346+btzmd-code@users.noreply.github.com>
2024-04-05 23:28:56 +0800

    Update script.js
    Updated function

--- script.js
+++ script.js
@@ -14,5 +14,36 @@ function toggleMenu() {
     container.addEventListener('mouseleave', () => video.pause()); // Pause video on mouseleave
   });
 
+const typedText = document.getElementById('typed-text');
+  const textArray = ["Front End Developer", "Software Engineer", "Web Dev", "Dev Ops"]; // Replace with your desired text options
+  let currentTextIndex = 0;
+  let typingSpeed = 150; // Adjust typing speed (milliseconds)
+  let isDeleting = false;
+  
+  function typeWriter() {
+    const currentText = textArray[currentTextIndex];
+  
+    if (isDeleting) {
+      // Backspace functionality
+      typedText.textContent = currentText.substring(0, typedText.textContent.length - 1);
+      if (typedText.textContent === '') {
+        isDeleting = false;
+        currentTextIndex++;
+        if (currentTextIndex === textArray.length) currentTextIndex = 0;
+      }
+    } else {
+      // Typing functionality
+      typedText.textContent += currentText.charAt(typedText.textContent.length);
+      if (typedText.textContent === currentText) {
+        isDeleting = true;
+      }
+    }
+  
+    setTimeout(typeWriter, isDeleting ? typingSpeed / 2 : typingSpeed);
+  }
+  
+  typeWriter();
+  
+
 
 
